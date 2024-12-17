d106f735bb4fa3c52b87c6b97f27595a9e022c2e
Baraa Basata <bbasata@gmail.com>
2024-12-17 18:41:36 -0500

    a test that fails to GetProviderSchema
    TF_ACC=1 go test -v ./...
    === RUN   TestProvider
    panic: not implemented
    
    goroutine 66 [running]:
    github.com/dappled-dawn/cloud-labs/terraform-provider-github.(*githubProvider).Schema(0x104fe2d50?, {0x14000524030?, 0x104c12c09?}, {}, 0x9?)
    	/Users/baraa.basata/src/labs/terraform-provider-github/provider.go:26 +0x2c
    github.com/hashicorp/terraform-plugin-framework/internal/fwserver.(*Server).ProviderSchema(0x1400039a6c8, {0x104fe2d50, 0x14000524030})
    	/Users/baraa.basata/go/pkg/mod/github.com/hashicorp/terraform-plugin-framework@v1.13.0/internal/fwserver/server.go:401 +0x16c
    github.com/hashicorp/terraform-plugin-framework/internal/fwserver.(*Server).GetProviderSchema(0x1400039a6c8, {0x104fe2d50, 0x14000524030}, 0x0?, 0x140001dd618)
    	/Users/baraa.basata/go/pkg/mod/github.com/hashicorp/terraform-plugin-framework@v1.13.0/internal/fwserver/server_getproviderschema.go:35 +0x80
    github.com/hashicorp/terraform-plugin-framework/internal/proto5server.(*Server).GetProviderSchema(0x1400039a6c8, {0x104fe2d50?, 0x14000369ef0?}, 0x1056d22a0

--- terraform-provider-github/go.mod
+++ terraform-provider-github/go.mod
@@ -4,6 +4,7 @@ go 1.23.3
 
 require (
 	github.com/hashicorp/terraform-plugin-framework v1.13.0
+	github.com/hashicorp/terraform-plugin-go v0.25.0
 	github.com/hashicorp/terraform-plugin-testing v1.11.0
 )
 
@@ -30,7 +31,6 @@ require (
 	github.com/hashicorp/logutils v1.0.0 // indirect
 	github.com/hashicorp/terraform-exec v0.21.0 // indirect
 	github.com/hashicorp/terraform-json v0.23.0 // indirect
-	github.com/hashicorp/terraform-plugin-go v0.25.0 // indirect
 	github.com/hashicorp/terraform-plugin-log v0.9.0 // indirect
 	github.com/hashicorp/terraform-plugin-sdk/v2 v2.35.0 // indirect
 	github.com/hashicorp/terraform-registry-address v0.2.3 // indirect
--- terraform-provider-github/provider_test.go
+++ terraform-provider-github/provider_test.go
@@ -1,11 +1,15 @@
 package main // todo: main_test
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-framework/providerserver"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
 )
 
 func TestProvider(t *testing.T) {
@@ -13,6 +17,38 @@ func TestProvider(t *testing.T) {
 		ProtoV5ProviderFactories: map[string]func() (tfprotov5.ProviderServer, error){
 			"github": providerserver.NewProtocol5WithError(providerFactory()),
 		},
-		Steps: []resource.TestStep{},
+		Steps: []resource.TestStep{
+			{
+				ResourceName:       "",
+				Taint:              []string{},
+				Config:             "data \"github_repository\" \"example\" {\n  repository = \"terraform-provider-github\"\n}\n\noutput \"example\" {\n  value = data.github_repository.example\n}\n",
+				Check:              func(*terraform.State) error { panic("not implemented") },
+				Destroy:            false,
+				ExpectNonEmptyPlan: false,
+				ExpectError:        &regexp.Regexp{},
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply:             []plancheck.PlanCheck{},
+					PostApplyPreRefresh:  []plancheck.PlanCheck{},
+					PostApplyPostRefresh: []plancheck.PlanCheck{},
+				},
+				RefreshPlanChecks: resource.RefreshPlanChecks{
+					PostRefresh: []plancheck.PlanCheck{},
+				},
+				ConfigStateChecks:                    []statecheck.StateCheck{},
+				PlanOnly:                             false,
+				PreventDiskCleanup:                   false,
+				PreventPostDestroyRefresh:            false,
+				ImportState:                          false,
+				ImportStateId:                        "",
+				ImportStateIdPrefix:                  "",
+				ImportStateIdFunc:                    func(*terraform.State) (string, error) { panic("not implemented") },
+				ImportStateCheck:                     func([]*terraform.InstanceState) error { panic("not implemented") },
+				ImportStateVerify:                    false,
+				ImportStateVerifyIdentifierAttribute: "",
+				ImportStateVerifyIgnore:              []string{},
+				ImportStatePersist:                   false,
+				RefreshState:                         false,
+			},
+		},
 	})
 }
